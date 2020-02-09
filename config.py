8c13166d7c0e8d6393ffba1f47082443b9e18233
JosePedro <josepedro88@live.com.pt>
2020-02-09 20:56:42 +0000

    Revert DB creation

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
 import os
 
 class Config(object):
-<<<<<<< HEAD
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'guess-it'
-    SQLALCHEMY_DATABASE_URI = os.environ['postgresql://postgres:2427@localhost/bugTracker']
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-=======
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
->>>>>>> parent of b38ccec... sqlite and alembic migration setup
