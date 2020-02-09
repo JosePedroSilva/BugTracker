54d69fb8e314d1f4bfce65523da0718357dd7c57
JosePedro <josepedro88@live.com.pt>
2020-02-09 12:24:46 +0000

    Base template html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Bug Tracker'
+    user = {'username': 'Joe'}
+    tickets = [
+        {
+            'ref': 'BT-1',
+            'desc': 'Bug description text'
+        },
+        {
+            'ref': 'BT-2',
+            'desc': "My app doesn't work anymore HELP"
+        }
+        ]
+    return render_template('index.html', title='Home', user=user, tickets=tickets)
