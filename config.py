fac9548894f048ecbe673bc6ddbd57bb9a3700e6
JosePedro <josepedro88@live.com.pt>
2020-02-10 10:04:15 +0000

    postgres and migration setup

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2427@localhost/bugTracker'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+
+
+    #postgresql+psycopg2://postgres:2427@localhost/bugTracker
