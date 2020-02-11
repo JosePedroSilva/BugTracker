4861915ce5109431cdda0510906fdf8cbaad3974
JosePedro <josepedro88@live.com.pt>
2020-02-11 10:04:39 +0000

    login time setup

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     tickets = db.relationship('Ticket', backref='author', lazy='dynamic')
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
